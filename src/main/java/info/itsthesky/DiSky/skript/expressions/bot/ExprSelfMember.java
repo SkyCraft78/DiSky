d8d05dea1a2a085e0172cd8b00ba81b4faeaab5f
Sky <livegogo.16334@gmail.com>
2021-06-01 12:32:42 +0200

    :zap: Fixed whole self member expression

--- src/main/java/info/itsthesky/DiSky/skript/expressions/bot/ExprSelfMember.java
+++ src/main/java/info/itsthesky/DiSky/skript/expressions/bot/ExprSelfMember.java
@@ -1,54 +1,64 @@
 package info.itsthesky.disky.skript.expressions.bot;
 
-import ch.njol.skript.classes.Changer;
+import ch.njol.skript.Skript;
 import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
-import ch.njol.skript.expressions.base.SimplePropertyExpression;
-import ch.njol.util.coll.CollectionUtils;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
 import info.itsthesky.disky.tools.Utils;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import org.bukkit.event.Event;
 
-import javax.annotation.Nullable;
+@Name("Self Member of Bot")
+@Description("Return the self member of a bot in a specific guild.")
+@Examples("set {_member} to self member of bot named \"MyBot\" in event-guild")
+@Since("1.4.2")
+public class ExprSelfMember extends SimpleExpression<Member> {
 
-@Name("Bot Self Member")
-@Description("Get the self member (member type) of a bot (JDA type)")
-@Since("1.11")
-public class ExprSelfMember extends SimplePropertyExpression<JDA, Member> {
+	static {
+		Skript.registerExpression(ExprSelfMember.class, Member.class, ExpressionType.SIMPLE,
+				"["+ Utils.getPrefixName() +"] [the] [(gateway|rest)] self member of [the] %bot% in [the] [guild] %guild%");
+	}
 
-    static {
-        register(ExprSelfMember.class, Member.class,
-                "[bot] self member",
-                "string/bot"
-        );
-    }
+	private Expression<JDA> exprBot;
+	private Expression<Guild> exprGuild;
 
-    @Nullable
-    @Override
-    public Member convert(JDA entity) {
-        return Utils.searchMember(entity, entity.getSelfUser().getId());
-    }
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+		exprBot = (Expression<JDA>) exprs[0];
+		exprGuild = (Expression<Guild>) exprs[1];
+		return true;
+	}
 
-    @Override
-    public Class<? extends Member> getReturnType() {
-        return Member.class;
-    }
+	@Override
+	protected Member[] get(Event e) {
+		JDA bot = exprBot.getSingle(e);
+		Guild guild = exprGuild.getSingle(e);
+		if (bot == null || guild == null) return new Member[0];
+		return new Member[] {guild.getMember(bot.getSelfUser())};
+	}
 
-    @Override
-    protected String getPropertyName() {
-        return "self member";
-    }
+	@Override
+	public boolean isSingle() {
+		return true;
+	}
 
-    @Nullable
-    @Override
-    public Class<?>[] acceptChange(Changer.ChangeMode mode) {
-        return CollectionUtils.array();
-    }
+	@Override
+	public Class<? extends Member> getReturnType() {
+		return Member.class;
+	}
 
-    @Override
-    public void change(Event e, @Nullable Object[] delta, Changer.ChangeMode mode) {
+	@Override
+	public String toString(Event e, boolean debug) {
+		return "self member of " + exprBot.toString(e, debug) + " in guild " + exprGuild.toString(e, debug);
+	}
 
-    }
 }
