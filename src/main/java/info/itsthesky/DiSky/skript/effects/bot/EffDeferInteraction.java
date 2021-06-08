0718b0aa7c98e49c7df569698fa3b68ec6c6e0a4
Sky <livegogo.16334@gmail.com>
2021-06-08 19:27:44 +0200

    :sparkles: Added interaction acknowledge effect

---
+++ src/main/java/info/itsthesky/DiSky/skript/effects/bot/EffDeferInteraction.java
@@ -0,0 +1,68 @@
+package info.itsthesky.disky.skript.effects.bot;
+
+import ch.njol.skript.ScriptLoader;
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import info.itsthesky.disky.skript.events.skript.EventButtonClick;
+import info.itsthesky.disky.skript.events.skript.slashcommand.EventSlashCommand;
+import info.itsthesky.disky.skript.events.util.InteractionEvent;
+import info.itsthesky.disky.tools.AsyncEffect;
+import info.itsthesky.disky.tools.DiSkyErrorHandler;
+import info.itsthesky.disky.tools.Utils;
+import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
+import net.dv8tion.jda.api.events.interaction.GenericInteractionCreateEvent;
+import org.bukkit.event.Event;
+
+@Name("Defer Interaction")
+@Description({"Let discord you've been receiving the interaction (work in buttons and slash commands events only) ONE TIME!",
+"If you're replying in one of these event, the deferring is useless and you'll get an error.",
+"Replying will automatically deferring the interaction!"})
+@Examples("defer the interaction")
+@Since("1.2")
+public class EffDeferInteraction extends AsyncEffect {
+
+    static {
+        Skript.registerEffect(EffDeferInteraction.class,
+                "["+ Utils.getPrefixName() +"] (defer|acknowledge) [the] interaction [event]");
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+        if (!ScriptLoader.isCurrentEvent(EventButtonClick.class, EventSlashCommand.class)) {
+            Skript.error("Cannot defer an interaction in a non-interaction event! (slash command / button)");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected void execute(Event e) {
+        DiSkyErrorHandler.executeHandleCode(e, event -> {
+            GenericInteractionCreateEvent interaction;
+            try  {
+                interaction = ((InteractionEvent) event).getInteractionEvent();
+            } catch (ClassCastException exception) {
+                Skript.error("[DiSky] Cannot cast a non-interaction event in a defer effect! Excepted slash command or button event, but got "+e.getEventName()+" event!");
+                return;
+            }
+            if (interaction instanceof ButtonClickEvent) {
+                ((ButtonClickEvent) interaction).deferEdit().queue();
+            } else {
+                interaction.deferReply().queue();
+            }
+        });
+    }
+
+    @Override
+    public String toString(Event e, boolean debug) {
+        return "defer the interaction event";
+    }
+
+}
